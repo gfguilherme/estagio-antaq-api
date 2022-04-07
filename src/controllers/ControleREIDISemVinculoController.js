014f84be969e718e5b59291ed08e8a1bb9303172
Pedro Bernardes <pedroabernardes11@gmail.com>
2022-04-07 16:29:14 -0300

    refactor: change function

--- src/controllers/ControleREIDISemVinculoController.js
+++ src/controllers/ControleREIDISemVinculoController.js
@@ -3,7 +3,7 @@ const { extensaoControleDB } = require("../database");
 module.exports = {
   async read(req, res, next) {
     try {
-      const results = await knex("TBControleREIDISemVinculo");
+      const results = await extensaoControleDB("TBControleREIDISemVinculo");
 
       res.json(results);
     } catch (error) {}
