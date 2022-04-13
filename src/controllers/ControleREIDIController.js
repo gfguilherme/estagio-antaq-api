56c142c859c7bf1706fa2ef824cbab6528b2e9fc
Pedro Bernardes <pedroabernardes11@gmail.com>
2022-04-12 21:06:42 -0300

    feat: add create and delete TBControleREIDI

--- src/controllers/ControleREIDIController.js
+++ src/controllers/ControleREIDIController.js
@@ -10,4 +10,47 @@ module.exports = {
       next(error);
     }
   },
+
+  async create(req, res, next){
+    try {
+      const { IDControleREIDI,
+        IDContratoArrendamento,
+        NRProcessoPrincipal,
+        IDProtocoloSEI,
+        DTProtocoloPedido,
+        VLInvestimentoProposto,
+        DSObservacoesSituacao,
+        NRProtocoloMINFRA,
+        NRCodigoMINFRA } = req.body
+
+      await extensaoControleDB('TBControleREIDI').insert({
+        IDControleREIDI,
+        IDContratoArrendamento,
+        NRProcessoPrincipal,
+        IDProtocoloSEI,
+        DTProtocoloPedido,
+        VLInvestimentoProposto,
+        DSObservacoesSituacao,
+        NRProtocoloMINFRA,
+        NRCodigoMINFRA,
+      })
+
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async delete(req, res, next){
+    try {
+      const { IDControleREIDI } = req.params;
+
+      await extensaoControleDB("TBControleREIDI")
+      .where({IDControleREIDI})
+      .del();
+
+      res.send();
+    } catch (error) {
+      next(error);
+    }
+  }
 };
