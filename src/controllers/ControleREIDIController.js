1250e57ecea456fdae4807a63236df3d30152116
Pedro Bernardes <pedroabernardes11@gmail.com>
2022-04-13 10:09:09 -0300

    chore: add credentils to gitignore

--- src/controllers/ControleREIDIController.js
+++ src/controllers/ControleREIDIController.js
@@ -37,19 +37,17 @@ module.exports = {
         NRCodigoMINFRA,
       });
 
-      res
-        .status(201)
-        .json({
-          IDControleREIDI,
-          IDContratoArrendamento,
-          NRProcessoPrincipal,
-          IDProtocoloSEI,
-          DTProtocoloPedido,
-          VLInvestimentoProposto,
-          DSObservacoesSituacao,
-          NRProtocoloMINFRA,
-          NRCodigoMINFRA,
-        });
+      res.status(201).json({
+        IDControleREIDI,
+        IDContratoArrendamento,
+        NRProcessoPrincipal,
+        IDProtocoloSEI,
+        DTProtocoloPedido,
+        VLInvestimentoProposto,
+        DSObservacoesSituacao,
+        NRProtocoloMINFRA,
+        NRCodigoMINFRA,
+      });
     } catch (error) {
       next(error);
     }
@@ -63,7 +61,43 @@ module.exports = {
         .where({ IDControleREIDI })
         .del();
 
-      res.status(200).json({message: `O REIDI de ID ${IDControleREIDI} foi excluído com sucesso`});
+      res.status(200).json({
+        message: `O REIDI de ID ${IDControleREIDI} foi excluído com sucesso`,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+  async update(req, res, next) {
+    try {
+      const { IDControleREIDI } = req.params; 
+      const {
+        DTProtocoloPedido,
+        VLInvestimentoProposto,
+        DSObservacoesSituacao,
+        NRProtocoloMINFRA,
+        NRCodigoMINFRA,
+      } = req.body;
+
+      await extensaoControleDB("TBControleREIDI")
+        .where({ IDControleREIDI })
+        .update({
+          DTProtocoloPedido,
+          VLInvestimentoProposto,
+          DSObservacoesSituacao,
+          NRProtocoloMINFRA,
+          NRCodigoMINFRA,
+        });
+
+      res
+        .status(200)
+        .json({
+          DTProtocoloPedido,
+          VLInvestimentoProposto,
+          DSObservacoesSituacao,
+          NRProtocoloMINFRA,
+          NRCodigoMINFRA,
+        });
     } catch (error) {
       next(error);
     }
